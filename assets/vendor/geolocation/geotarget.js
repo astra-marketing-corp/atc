921fb2bfa2f0b26d8189ceab2a5f3b55a26d5d40
Cj Pascual <cjrpascual.official@gmail.com>
2025-09-28 07:22:22 +0800

    major updates

--- assets/vendor/geolocation/geotarget.js
+++ assets/vendor/geolocation/geotarget.js
@@ -37,7 +37,7 @@
         }
     }
 
-    const csvUrl = "../locations.csv";
+    const csvUrl = "../../locations.csv";
     const locations = await fetchLocationsCSV(csvUrl);
 
     const params = new URLSearchParams(window.location.search);
